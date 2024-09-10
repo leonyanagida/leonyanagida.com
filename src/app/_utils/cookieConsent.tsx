7a71b7cf393e97c3e260baae0e07a37f3669cb1f
leonyanagida <leonyanagida@gmail.com>
2024-09-09 21:20:28 -0700

    poll cookie consent

--- src/app/_utils/cookieConsent.tsx
+++ src/app/_utils/cookieConsent.tsx
@@ -11,10 +11,21 @@ const CookieConsent = ({ gaId }: Props) => {
   const [consentGiven, setConsentGiven] = useState(false);
 
   useEffect(() => {
-    const consent = localStorage.getItem("cookieConsent");
-    if (consent === "accepted") {
-      setConsentGiven(true);
-    }
+    const checkConsent = () => {
+      const consent = localStorage.getItem("cookieConsent");
+      if (consent === "accepted") {
+        setConsentGiven(true);
+        clearInterval(interval); // Stop polling when consent is given
+      }
+    };
+
+    // Check for consent immediately on mount
+    checkConsent();
+
+    // Set an interval to re-check for consent in case it is set later by the GTM banner
+    const interval = setInterval(checkConsent, 1000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
